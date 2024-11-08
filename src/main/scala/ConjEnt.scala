f1dc27792c690accc2ad098280b6d882b8bee1a0
Felipe Rivas <79608891+morado123@users.noreply.github.com>
2024-11-08 12:38:04 -0500

    Ajuste del proyecto

--- src/main/scala/ConjEnt.scala
+++ src/main/scala/ConjEnt.scala
@@ -2,4 +2,6 @@ abstract class ConjEnt {
   def pertenece(x: Int): Boolean
   def insertar(x: Int): ConjEnt
   def eliminar(x: Int): ConjEnt
+
+
 }
--- src/main/scala/ConjFactory.scala
+++
@@ -1,8 +0,0 @@
-object ConjFactory {
-  def crearConjunto(tipo: String): ConjEnt = tipo match {
-    case "ArbolBinario" => Vacio
-    case "Lista" => new ConjLista(Nil)
-    case "AVL" => new ConjAVL(0, Vacio, Vacio) // Proporcionar un nodo inicial para AVL
-    case _ => throw new IllegalArgumentException("Tipo de conjunto no soportado")
-  }
-}
